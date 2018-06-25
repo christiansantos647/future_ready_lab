019eaed8b94dfe78d56e261cb976b4b52c62d4ad
christiansantos647 <cesantos647@gmail.com>
2018-06-25 13:47:25 -0700

    Finished number guessing version 1

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,1 +1,54 @@
 /*eslint-env browser*/
+function playGame() {
+    
+
+    alert("First, enter a low number, then a high number. Then, guess a random number within the low and high number");
+
+    //Obtains the bounds of the game
+    //Uses parseInt() to ensure that numbers rather than strings are used
+
+    var from = parseInt(prompt("Enter the lower bound"));
+
+        while (typeof from == "number") {
+            from = parseInt(prompt("That is not a real number. Please enter the lower bound"));
+        }
+
+    var to = parseInt(prompt("Enter the higher bound"));
+
+            while (typeof to == "number") {
+
+            to = parseInt(prompt("That is not a real number. Please enter the higher bound"));
+        }
+
+    //Use Math.round() to get whole number
+    var target = Math.round((Math.random() * (to - from)) + from);
+
+    var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
+
+    var totalGuesses = 1
+while (target != currentGuess) {
+    if (currentGuess > from && currentGuess < to) {
+
+        if (currentGuess < target) {
+
+            currentGuess = parseInt(prompt("Enter a higher number than " + currentGuess));
+
+            totalGuesses++;
+
+        } else if (currentGuess > target) {
+
+            currentGuess = parseInt(prompt("Enter a lower number than " + currentGuess));
+
+            totalGuesses++;
+
+        } 
+
+    } else {
+
+        currentGuess = parseInt(prompt("Please enter a number within the range"));
+
+    }
+
+}
+    alert("Congrats! You guessed the number in " + totalGuesses + " guesses!");
+}
