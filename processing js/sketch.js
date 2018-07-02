0f5b89df527789fe9f32f8826f831366010db851
christiansantos647 <cesantos647@gmail.com>
2018-07-02 14:01:48 -0700

    Added comments to the code

--- processing js/sketch.js
+++ processing js/sketch.js
@@ -15,25 +15,40 @@ function setup() {
 }
 
 function draw() {
-  fill(circle.color);
+    //Creates the circle that will be drawn
+    fill(circle.color);
+    
     ellipse(circle.xCoor, circle.yCoor, circle.diameter);
     
-    //if we hit the right edge of the canvas
+    //Checks if circle is touching an edge and changes its direction
     if(circle.xCoor > 640 || circle.xCoor < 0){
-        circle.color = [random(0,255),random(0,255),random(0,255)]//makes the circle black
-        circle.xSpeed = -circle.xSpeed;//changes speed of the circle
-        circle.diameter = random(40,80);
-    } else if(circle.yCoor > 480 || circle.yCoor < 0){
-        circle.color = [random(0,255),random(0,255),random(0,255)];
-        circle.ySpeed = -circle.ySpeed;
-        circle.diameter = random(40,80);
         
+        circle.color = [random(0,255),random(0,255),random(0,255)]//makes the circle a random color
+        
+        circle.xSpeed = -circle.xSpeed;//changes speed of the circle
+
+        circle.diameter = random(40,80);//Changes the size of the code
+        
+    } else if(circle.yCoor > 480 || circle.yCoor < 0){
+        
+        circle.color = [random(0,255),random(0,255),random(0,255)];
+        
+        circle.ySpeed = -circle.ySpeed;//Changes the speed of the code
+        
+        circle.diameter = random(40,80);//Changes the size of the code
+        
+    //If not touching an edge, circle keeps moving forward    
     } else {
+        
         circle.xSpeed = circle.xSpeed;
+        
         circle.ySpeed = circle.ySpeed;
+        
     }
     
     circle.xCoor += circle.xSpeed;
+    
     circle.yCoor += circle.ySpeed;
+    
 }
 
