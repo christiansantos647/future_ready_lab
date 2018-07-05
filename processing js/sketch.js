a1597d8a4bb525d6c7c72faae230c4f56629f695
christiansantos647 <cesantos647@gmail.com>
2018-07-05 14:41:14 -0700

    added functions to shorten repetitive functions and to bounce balls

--- processing js/sketch.js
+++ processing js/sketch.js
@@ -1,41 +1,3 @@
-//setup is run once at the beginning before we draw
-/* var circle = {
-    diameter: 80,
-    xCoor: 0,
-    yCoor: 0,
-    color: [255,0,0], //RGB values give circle a red color
-    xSpeed: 5,
-    ySpeed: 5
-} */
-
-//Creation of a class for the circle
-function circle(x, y, size, color, xSpeed, ySpeed) {
-    
-    this.xCoor = x;
-    
-    this.yCoor = y;
-    
-    this.diameter = size;
-    
-    this.color = color || [0,0,0];//Allows the user to choose a color or have the circle be black
-    
-    this.xSpeed = xSpeed || 13;//User can set speed or choose a default horizontal speed
-    
-    this.ySpeed = ySpeed || 9;//User can set speed ot choose a default vertical speed
-}
-
-/*
-
-var Circle = new circle(0,0,80,[255,0,0],10,13);
-
-var circle2 = new circle(0,30,50);
-
-*/
-var circleList = [];
-
-//var circleNumber = [2,4,6,8]
-
-
 //Allows user to set the width of the screen
 var canvasWidth = parseInt(prompt("Please enter the width of the screen"));
 
@@ -56,24 +18,58 @@ while(canvasHeight < 0 || isNaN(canvasHeight)) {
 
 }
 
+//Creation of a class for the circle
+function circle(x, y, size, color, xSpeed, ySpeed) {
+    
+    this.xCoor = x;
+    
+    this.yCoor = y;
+    
+    this.diameter = size;
+    
+    this.color = color || [0,0,0];//Allows the user to choose a color or have the circle be black
+    
+    this.xSpeed = xSpeed || 13;//User can set speed or choose a default horizontal speed
+    
+    this.ySpeed = ySpeed || 9;//User can set speed ot choose a default vertical speed
+}
+
+//reverses the horizontal speed of the circle
+function changeHorizontalSpeed(i) {
+    
+    circleList[i].xSpeed = -circleList[i].xSpeed;//changes speed of the circle
+    
+}
+
+//reverses the vertical speed of the circle
+function changeVerticalSpeed(i) {
+    
+    circleList[i].ySpeed = -circleList[i].ySpeed;//changes speed of the circle
+    
+}
+
+//creates new parameters for the circle
+function randomize(i) {
+    
+    circleList[i].color = [random(0,255),random(0,255),random(0,255)];//makes the circle a random color
+    
+    circleList[i].diameter = random(40,80);//Changes the size of the code
+    
+}
+
+var circleList = [];
+
 //Options for the number of balls on the screen
 var balls = [2,5,9,16];
-
 
 function setup() {
     
     createCanvas(canvasWidth, canvasHeight);//Creates the screen for the animation
     
-    //background(255,255,255);
-    
-    //Regulates how fast the animation is going
-    frameRate(100);
-    
-    //Creates a number that will determine which index in the array "balls" will be used to decide the number of balls used in the animation
-    ballOption = round(random(0,3));
+    frameRate(100);//Regulates how fast the animation is going
     
     //adds the number of balls needed for the animation
-    for(var ballNumber = 0; ballNumber < balls[ballOption]; ballNumber++){
+    for(var ballNumber = 0; ballNumber < balls[round(random(0,3))]; ballNumber++){
     
         //Adds a new circle to the array "balls" with random parameters that adhere to the boundaries of the screen
         circleList.push(new circle(random(0, canvasWidth), random(0,canvasHeight), random(0,100),[random(0,255),random(0,255),random(0,255)],random(1,20),random(1,20)));
@@ -92,61 +88,66 @@ function draw() {
         //Creates the circle that will be drawn
         fill(circleList[i].color);
 
-        ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter );
+        ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter);
         
-/*
-        //Checks if the circle is touching a corner 
-        if(circleList[i].xCoor > canvasWidth && circleList[i].yCoor > canvasHeight || circleList[i].xCoor > canvasWidth && circleList[i].yCoor < 0 || circleList.xCoor < canvasWidth && circleList < canvasHeight || circleList[i].xCoor < 0 && circleList[i].yCoor < 0) {
-            
-            circleList[i].color = [random(0,255),random(0,255),random(0,255)];//makes the circle a random color
-
-            circleList[i].xSpeed = -circleList[i].xSpeed;//changes speed of the circle
-            
-            circleList[i].ySpeed = -circleList[i].ySpeed;
-
-            circleList[i].diameter = random(40,80);//Changes the size of the code
-        }
-*/        
- /*
         //Checks if the circle is touching any other circle
         for(var z = 0; z < circleList.length; z++){
             
+            requiredDistance = (circleList[z].diameter + circleList[i].diameter)/2;
+            
             if(circleList[i] == circleList[z]) {
                 
-                circleList[i].xSpeed = circleList[i].xSpeed;
-
-                circleList[i].ySpeed = circleList[i].ySpeed;
+                //moves onto the next object
+                z++;
                 
-            } else if(circleList[i].xCoor == circleList[z].xCoor && circleList[i].yCoor == circleList[z].yCoor) {
+            } else if(dist(circleList[i].xCoor, circleList[i].yCoor, circleList[z].xCoor, circleList[z].yCoor) < requiredDistance) {
                 
-                circleList[i].color = [random(0,255),random(0,255),random(0,255)];//makes the circle a random color
+                randomize(i);
+                
+                //changes the speed so that both balls move at a new speed
+               /* var newHorizontalSpeed = (circleList[z].xSpeed + circleList[i].xSpeed) / 2;
+                
+                var newVerticalSpeed = (circleList[z].ySpeed + circleList[i].ySpeed) / 2;
+                //changes the speed of both 
+                circleList[z].xSpeed = newVerticalSpeed;
+                
+                circleList[z].xSpeed = newHorizontalSpeed;
+                
+                circleList[i].xSpeed = newVerticalSpeed;
+                
+                circleList[i].xSpeed = newHorizontalSpeed;
+                    */
+                
+                //function to bounce the ball
+                changeHorizontalSpeed(z);
 
-                circleList[i].xSpeed = -circleList[i].xSpeed;//changes speed of the circle
+                changeVerticalSpeed(z);
+                
+                changeHorizontalSpeed(i);
+                
+                changeVerticalSpeed(i);
+                
+                circleList[z].xCoor += circleList[z].xSpeed;
+                
+                circleList[z].yCoor += circleList[z].ySpeed;
 
-                circleList[i].ySpeed = -circleList[i].ySpeed;
 
-                circleList[i].diameter = random(40,80);//Changes the size of the code
             }
             
-        }    
-*/        
+        }          
         
         //Checks if circle is touching an edge and changes its direction
-        if(circleList[i].xCoor > canvasWidth || circleList[i].xCoor < 0){
+        if(circleList[i].xCoor > width || circleList[i].xCoor < 0){
 
-            circleList[i].color = [random(0,255),random(0,255),random(0,255)];//makes the circle a random color
+            randomize(i);
 
-            circleList[i].xSpeed = -circleList[i].xSpeed;//changes speed of the circle
+            changeHorizontalSpeed(i);
 
-            circleList[i].diameter = random(40,80);//Changes the size of the code
+        } else if(circleList[i].yCoor > height || circleList[i].yCoor < 0){
 
-        } else if(circleList[i].yCoor > canvasHeight || circleList[i].yCoor < 0){
+            randomize(i);
 
-            circleList[i].color = [random(0,255),random(0,255),random(0,255)];//makes the circle a random color
-
-            circleList[i].ySpeed = -circleList[i].ySpeed;//Changes the speed of the code
-
-            circleList[i].diameter = random(40,80);//Changes the size of the code
+            changeVerticalSpeed(i);
 
         //If not touching an edge, circle keeps moving forward    
         } else {
@@ -165,4 +166,3 @@ function draw() {
    }
    
 }
-
